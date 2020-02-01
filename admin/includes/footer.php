127b0209c5d40bf015d28e616cc6c36cef7d103a
Kumar Raosaheb Pandule <54455284+kumarpandule2000@users.noreply.github.com>
2020-02-01 10:16:12 -0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Government Polytecnic,Awsari(kh) Library Management System |<a  href="https://phpgurukul.com/" target="_blank"  > Designed by : Kumar Pandule.</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>  
