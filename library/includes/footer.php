83beee1f9579911e879c80bee53de4a847db057e
Kumar Pandule <54455284+kumarpandule2000@users.noreply.github.com>
2021-06-28 21:58:37 +0530

    Add files via upload

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Library Management System |<a href="https://github.com/kumarpandule2000" target="_blank"  > Designed by : Kumar Pandule</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
